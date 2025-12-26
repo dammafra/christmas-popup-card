ad62eac8c4d3e1fc5fbf648a83d6bfa724a3bb78
Francesco Dammacco <francescodammacco@live.it>
2025-12-26 13:00:57 +0100

    Animate glow

--- src/components/Glow.tsx
+++ src/components/Glow.tsx
@@ -1,21 +1,24 @@
+import { a, useSpring } from '@react-spring/three'
 import { Billboard, GradientTexture, GradientType } from '@react-three/drei'
 
 export function Glow() {
+  const { opacity } = useSpring({ from: { opacity: 0 }, to: { opacity: 0.8 }, delay: 1500 })
+
   return (
     <Billboard>
-      <mesh position-z={-5} scale={5}>
+      <mesh position-y={2} position-z={-5} scale={5}>
         <circleGeometry />
-        <meshBasicMaterial>
+        <a.meshBasicMaterial transparent opacity={opacity}>
           <GradientTexture
             stops={[0, 1]}
-            colors={['#7b3306', 'black']}
+            colors={['#e17100', 'black']}
             size={512}
             width={512}
             type={GradientType.Radial}
-            innerCircleRadius={100}
-            outerCircleRadius={250}
+            innerCircleRadius={0}
+            outerCircleRadius={200}
           />
-        </meshBasicMaterial>
+        </a.meshBasicMaterial>
       </mesh>
     </Billboard>
   )
